5812191390ca8157da631ee0c7e553387c3a12f1
Avan <layouwen@gmail.com>
2022-06-20 23:55:26 +0800

    feat: add base Button style

--- src/button/src/button.tsx
+++ src/button/src/button.tsx
@@ -1,11 +1,16 @@
-import { defineComponent } from "vue";
+import { defineComponent, toRefs } from "vue";
+import { buttonProps, ButtonProps } from "./button-type";
 
 export default defineComponent({
   name: "FkButton",
-  setup(props, { slots }) {
+  props: buttonProps,
+  setup(props: ButtonProps, { slots }) {
+    const { type } = toRefs(props);
     return () => {
       const defaultSlot = slots.default?.() || "按钮";
-      return <button>{defaultSlot}</button>;
+      return (
+        <button class={`fk-btn fk-btn--${type.value}`}>{defaultSlot}</button>
+      );
     };
   },
 });
