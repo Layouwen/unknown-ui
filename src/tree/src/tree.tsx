508a21489c41a78eec341c3aac736dce5b15fcee
Avan <layouwen@gmail.com>
2022-07-07 22:08:06 +0800

    feat: 展开收缩功能

--- src/tree/src/tree.tsx
+++ src/tree/src/tree.tsx
@@ -9,10 +9,36 @@ export default defineComponent({
     const { data } = toRefs(props);
     const innerData = ref(generateInnerTree(data.value));
 
-    // TODO: 筛选出只需要展示的节点
     const expendedTree = computed(() => {
-      return innerData.value;
+      const result = [] as IInnerTreeNode[];
+      // 将需要收缩的父节点中所有子节点存入数组，循环的时候排除它
+      let excludeNode = [] as IInnerTreeNode[];
+      for (const node of innerData.value) {
+        if (excludeNode.includes(node)) continue;
+
+        // 如果该节点需要收缩，则获取他所有子节点
+        if (!node.expanded) {
+          excludeNode = getChildren(node);
+        }
+        result.push(node);
+      }
+      return result;
     });
+
+    const getChildren = (node: IInnerTreeNode) => {
+      const result = [] as IInnerTreeNode[];
+      // 找到该节点开始位置
+      const index = innerData.value.findIndex((i) => i === node);
+      // 往后遍历找到所有子节点
+      for (
+        let i = index + 1;
+        i < innerData.value.length && node.level < innerData.value[i].level;
+        i++
+      ) {
+        result.push(innerData.value[i]);
+      }
+      return result;
+    };
 
     const toggleExpend = (node: IInnerTreeNode) => {
       const cur = innerData.value.find((item) => item.id === node.id);
